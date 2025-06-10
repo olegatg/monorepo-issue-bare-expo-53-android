1f3c6b7efac045cbf3a3d43432a3f69ac622ede4
Olegas Kosuchinas <olegas.kosuchinas@atg.se>
2025-06-10 21:52:47 +0200

    Adjust metro.config.js file as per monorepo documentation

--- apps/mobileapp/AwesomeProject/metro.config.js
+++ apps/mobileapp/AwesomeProject/metro.config.js
@@ -1,12 +1,19 @@
-const { getDefaultConfig } = require('expo/metro-config');
-const { mergeConfig } = require('@react-native/metro-config');
+const {getDefaultConfig} = require('expo/metro-config');
+const path = require('path');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+// Find the project and workspace directories
+const projectRoot = __dirname;
+// we are in apps/mobileapp/AwesomeProject, so we need to go up three levels to reach the monorepo root
+const monorepoRoot = path.resolve(projectRoot, '../../..');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = getDefaultConfig(projectRoot);
+
+// 1. Watch all files within the monorepo
+config.watchFolders = [monorepoRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+
+module.exports = config;
